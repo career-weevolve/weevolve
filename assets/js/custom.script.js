2cfd4f49c6d9a53eb6cec511c82c1e10102aaa57
career-weevolve <55353796+career-weevolve@users.noreply.github.com>
2019-09-17 20:38:23 +0800

    update

--- assets/js/custom.script.js
+++ assets/js/custom.script.js
@@ -26,7 +26,25 @@
         /*=========================================================================
          ===  MENU SCROLL FIXED END
          ========================================================================== */
-
+         (function($){
+            const videos = document.querySelectorAll("video")
+            videos.forEach(video => {
+              video.addEventListener("mouseover", function () {
+                this.play()
+              })
+              video.addEventListener("mouseout", function () {
+                this.pause()
+              })
+              video.addEventListener("touchstart", function () {
+                this.play()
+              })
+              video.addEventListener("touchend", function () {
+                this.pause()
+              })
+            })
+        
+        })(jQuery);
+        
 
 
 
